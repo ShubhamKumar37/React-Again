afe181fb9665674416f0dd30412ae1a2c4fd75e0
ShubhamKumar37 <sk9818281820@gmail.com>
2024-10-24 20:23:26 +0530

    5 component added for button and input filed and other updates

---
+++ project-blog-app/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = ({width = '100%'}) => {
+  return (
+    <img src='' alt='Logo placeholder' style={{width}} ></img>
+  )
+}
+
+export default Logo
--- project-blog-app/src/main.jsx
+++ project-blog-app/src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     element: <App />,
     children:[
       {
-        path: "/",
+        index: true,
         element: <Home />
       },
       {
