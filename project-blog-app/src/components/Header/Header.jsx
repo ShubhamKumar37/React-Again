c74ad021097e7b3cdecdf693116ca15af786530e
ShubhamKumar37 <sk9818281820@gmail.com>
2024-10-25 00:34:07 +0530

    Nav bar is almost completed need to understand why we did few things as they are very high quality code so understand it and make it in a practice

---
+++ project-blog-app/src/components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import Container from '../Container/Container';
+import LogoutBtn from './LogoutBtn';
+
+const Header = () => {
+
+    const authStatus = useSelector((state) => state.auth.status);
+    const navigate = useNavigate();
+
+    const navItem = [
+        {
+            name: "Home",
+            slug: "/",
+            active: true,
+        },
+        {
+            name: "All Posts",
+            slug: "/all-posts",
+            active: authStatus,
+        },
+        {
+            name: "Add Post",
+            slug: "/add-post",
+            active: authStatus,
+        },
+        {
+            name: "Signup",
+            slug: "/signup",
+            active: !authStatus,
+        },
+        {
+            name: "Login",
+            slug: "/login",
+            active: !authStatus, // Based on login or logout status we will decided wheather to display this or not
+        },
+    ];
+
+  return (
+    <header className='py-3 shadow bg-gray-500'>
+        <Container>
+            <nav className='flex'>
+                <div>
+                    <Link to='/'>
+                        {/* <Logo /> */}
+                        <img src='https://res.cloudinary.com/dtxu5ha3c/image/upload/v1721330403/Certification/32ecca85-3495-49aa-afa2-cc1b07003fd9.png' alt='Logo'></img>
+                    </Link>
+                </div>
+
+                <ul className='ml-auto'>
+                    {
+                        navItem.map((item, index) => item.active ? 
+                        <li key={index}>
+                            <button
+                                className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                            onClick={() => navigate(item.slug)}>{item.name}</button>
+                        </li>: null)
+                    }
+                    {
+                        authStatus && (<li>
+                            <LogoutBtn />
+                        </li>)
+                    }
+                </ul>
+            </nav>
+        </Container>
+    </header>
+  )
+}
+
+export default Header
---
+++ project-blog-app/src/components/Select.jsx
@@ -0,0 +1,34 @@
+import React, { useId } from 'react'
+
+const Select = ({
+    options,
+    label,
+    className,
+    ...props
+}, ref) => {
+    
+    const id = useId();
+
+  return (
+    <div>
+        {label && (
+            <label htmlFor={id} className='inline-block mb-1 pl-1'>{label}</label>
+        )}
+
+        <select
+            ref={ref}
+            {...props}
+            id={id}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-green-50 duration-200 border border-gray-200 w-full ${className}`}
+        >
+            {
+                options.map((item, index) =>(
+                    <option value={item} key={index}>{item}</option>
+                ))
+            }
+        </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select);
